e0e89c30a4e13bd2750a7f4199bf72082e840ece
choi-mk <xhrlmin10@naver.com>
2025-03-28 15:08:05 +0900

    feat: habitdone api 추가

--- backend/src/modules/index.module.js
+++ backend/src/modules/index.module.js
@@ -35,14 +35,26 @@ export default router;
 =======
 import express from "express";
 import healthCheckRouter from "./healthCheck.module.js";
+<<<<<<< HEAD
 import studyVerifyPassword from "./editStudyModules/studyVerifyPassword.module.js";
 import studyUpdate from "./editStudyModules/studyUpdate.module.js";
+=======
+import studyRouter from "./route/study.module.js";
+import focusRouter from "./route/focus.module.js";
+import habitRouter from "./route/habit.module.js";
+>>>>>>> 19fa891 (refactor: 라우터 세부라우터로 분리)
 
 const router = express.Router();
 
 router.use("/health-check", healthCheckRouter);
+<<<<<<< HEAD
 router.use("/api", studyVerifyPassword); // http://localhost:5090/api/verify-password
 router.use("/api", studyUpdate); // http://localhost:5090/api/study/{studyId}/update
+=======
+router.use("/api", studyRouter); // http://localhost:5090/api/구현study라우터명/...
+router.use("/api", focusRouter); // http://localhost:5090/api/구현focus라우터명/...
+router.use("/api", habitRouter); // http://localhost:5090/api/구현habit라우터명/...
+>>>>>>> 19fa891 (refactor: 라우터 세부라우터로 분리)
 
 export default router;
 >>>>>>> 83b45e2 (feat: 스터디 업데이트 모듈 구현)
